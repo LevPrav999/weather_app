67666f694d195067a4d445083c0f50c10bc9135b
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-07-29 18:27:00 +0300

    remove MainActivity

--- app/src/main/java/ru/levprav/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/ru/levprav/weatherapp/presentation/MainActivity.kt
@@ -1,4 +1,17 @@
-package ru.levprav.weatherapp.presentation.theme
+package ru.levprav.weatherapp.presentation
 
-class MainActivity {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import ru.levprav.weatherapp.presentation.ui.theme.WeatherAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WeatherAppTheme {
+
+            }
+        }
+    }
 }
