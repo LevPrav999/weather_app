c75e40df4ed60af0beaafa3615b86b1ee769a2f1
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-07-30 16:55:44 +0300

    add remote data source

---
+++ app/src/main/java/ru/levprav/weatherapp/data/remote/WeatherApi.kt
@@ -0,0 +1,13 @@
+package ru.levprav.weatherapp.data.remote
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherApi {
+
+    @GET("v1/forecast?hourly=temperature_2m,weathercode,relativehumidity_2m,windspeed_10m,pressure_msl")
+    suspend fun getWeather(
+        @Query("latitude") lat: Double,
+        @Query("longitude") long: Double
+    ): WeatherDto
+}
