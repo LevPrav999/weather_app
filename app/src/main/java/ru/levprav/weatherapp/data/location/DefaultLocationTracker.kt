424af4e4af26d04e825aab69944f6bcc2aa0024f
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-07-31 22:22:36 +0300

    update location tracker

--- app/src/main/java/ru/levprav/weatherapp/data/location/DefaultLocationTracker.kt
+++ app/src/main/java/ru/levprav/weatherapp/data/location/DefaultLocationTracker.kt
@@ -1,30 +1,31 @@
 package ru.levprav.weatherapp.data.location
 
+
+import android.Manifest
 import android.app.Application
 import android.content.Context
+import android.content.pm.PackageManager
 import android.location.Location
+import android.location.LocationManager
 import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
-import android.Manifest
-import android.content.pm.PackageManager
-import android.location.LocationManager
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.suspendCancellableCoroutine
 import ru.levprav.weatherapp.domain.location.LocationTracker
 import javax.inject.Inject
 import kotlin.coroutines.resume
 
+@ExperimentalCoroutinesApi
 class DefaultLocationTracker @Inject constructor(
     private val locationClient: FusedLocationProviderClient,
     private val application: Application
 ): LocationTracker {
-
 
     override suspend fun getCurrentLocation(): Location? {
         val hasAccessFineLocationPermission = ContextCompat.checkSelfPermission(
             application,
             Manifest.permission.ACCESS_FINE_LOCATION
         ) == PackageManager.PERMISSION_GRANTED
-
         val hasAccessCoarseLocationPermission = ContextCompat.checkSelfPermission(
             application,
             Manifest.permission.ACCESS_COARSE_LOCATION
@@ -33,15 +34,16 @@ class DefaultLocationTracker @Inject constructor(
         val locationManager = application.getSystemService(Context.LOCATION_SERVICE) as LocationManager
         val isGpsEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER) ||
                 locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-        if(!hasAccessCoarseLocationPermission || !hasAccessFineLocationPermission || !isGpsEnabled){
-            return null;
+        if(!hasAccessCoarseLocationPermission || !hasAccessFineLocationPermission || !isGpsEnabled) {
+            return null
         }
+
         return suspendCancellableCoroutine { cont ->
             locationClient.lastLocation.apply {
-                if(isComplete){
-                    if(isSuccessful){
+                if(isComplete) {
+                    if(isSuccessful) {
                         cont.resume(result)
-                    }else{
+                    } else {
                         cont.resume(null)
                     }
                     return@suspendCancellableCoroutine
@@ -57,6 +59,6 @@ class DefaultLocationTracker @Inject constructor(
                 }
             }
         }
-    }
 
+    }
 }
