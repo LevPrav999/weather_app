c949db8074c6bafca84eed1acfe546e6c995b9ef
LevPrav <68643465+LevPrav999@users.noreply.github.com>
2023-07-31 22:22:52 +0300

    update dependency injection

--- app/src/main/java/ru/levprav/weatherapp/di/LocationModule.kt
+++ app/src/main/java/ru/levprav/weatherapp/di/LocationModule.kt
@@ -4,13 +4,16 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import ru.levprav.weatherapp.data.location.DefaultLocationTracker
 import ru.levprav.weatherapp.domain.location.LocationTracker
 import javax.inject.Singleton
 
+@ExperimentalCoroutinesApi
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class LocationModule {
+
     @Binds
     @Singleton
     abstract fun bindLocationTracker(defaultLocationTracker: DefaultLocationTracker): LocationTracker
--- app/src/main/java/ru/levprav/weatherapp/di/RepositoryModule.kt
+++ app/src/main/java/ru/levprav/weatherapp/di/RepositoryModule.kt
@@ -4,14 +4,19 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import ru.levprav.weatherapp.data.repository.WeatherRepositoryImpl
 import ru.levprav.weatherapp.domain.repository.WeatherRepository
 import javax.inject.Singleton
 
+@ExperimentalCoroutinesApi
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
+
     @Binds
     @Singleton
-    abstract fun bindLocationRepository(weatherRepositoryImpl: WeatherRepositoryImpl): WeatherRepository
+    abstract fun bindWeatherRepository(
+        weatherRepositoryImpl: WeatherRepositoryImpl
+    ): WeatherRepository
 }
